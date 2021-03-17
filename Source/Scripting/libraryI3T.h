73aebd259f9e9ac11dab31b178291c9b16cfc84a
Daniel Gruncl <gruncdan@fel.cvut.cz>
2021-03-17 11:42:47 +0100

    moved platformI3T from picoc to scripting

--- Source/Scripting/libraryI3T.h
+++ Source/Scripting/libraryI3T.h
@@ -2,13 +2,15 @@
 #include <vector>
 #include <memory>
 #include "pgr.h"
-#include "GUI/Elements/Nodes/WorkspaceMatrix4x4.h"
 
+struct Mat4Types {
+	const int free = 0, scale = 1, uniscale = 2, rotatex = 3, rotatey = 4, rotatez = 5, translate = 6;
+};
 struct NodeMat4 {
-	Core::Transform::DataMap type;
+	int type;
 	glm::mat4 data;
 	int x, y;
-	NodeMat4(Core::Transform::DataMap _type,glm::mat4 _data, int _x, int _y) {type=_type;data=_data;x=_x;y=_y;}
+	NodeMat4(int _type,glm::mat4 _data, int _x, int _y) {type=_type;data=_data;x=_x;y=_y;}
 };
 struct NodeNormVec4 {
 	glm::vec4 data;
@@ -21,6 +23,7 @@ struct NodePlug {
 	NodePlug(int _a, int _b,int _pina,int _pinb ){ indexA =_a; indexB =_b; indexPinA =_pina; indexPinB =_pinb;}
 };
 struct WorkspaceLayout {
+	Mat4Types mat4Types;
 	std::vector<NodeMat4>mat4Nodes;
 	std::vector<NodeNormVec4>normVec4Nodes;
 	std::vector<NodePlug>nodePlugs;
@@ -29,3 +32,5 @@ struct WorkspaceLayout {
 
 WorkspaceLayout*getWorkspaceLayout();
 void clearWorkspaceLayout();
+
+void PlatformLibraryInitI3T(Picoc* pc);
