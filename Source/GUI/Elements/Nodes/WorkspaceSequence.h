ebefec5b70f3f7889d63a737f999c5b252c3a644
Jaroslav Holeček <holecj14@fel.cvut.cz>
2021-05-06 19:36:34 +0200

    mino fix

--- Source/GUI/Elements/Nodes/WorkspaceSequence.h
+++ Source/GUI/Elements/Nodes/WorkspaceSequence.h
@@ -20,7 +20,7 @@ protected:
     std::vector<Ptr<WorkspaceNodeWithCoreData>> m_workspaceTransformation;
 public:
 	WorkspaceSequence(ImTextureID headerBackground, WorkspaceSequenceArgs const& args);
-    WorkspaceSequence(ImTextureID headerBackground, std::string headerLabel = "default Sequence header", std::string nodeLabel = "Sequence", , Ptr<Core::Sequence> nodebase = nullptr);
+    WorkspaceSequence(ImTextureID headerBackground, std::string headerLabel = "default Sequence header", std::string nodeLabel = "Sequence", Ptr<Core::Sequence> nodebase = nullptr);
 
     void setPostionOfDummyData(int positionOfDummyData = -1);
     void setWidthOfDummy(int width = 0);
