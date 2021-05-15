9848c05a4a62dfee56a209e5e641634a21175e34
Martin Herich <martin.herich@skaut.cz>
2021-05-15 18:57:09 +0200

    Fix getValue from YAML::Node template

--- Source/Loader/Utils.h
+++ Source/Loader/Utils.h
@@ -1,10 +1,8 @@
 #include "yaml-cpp/yaml.h"
 
-template <typename Node, typename T>
-void getValue(Node&& node, T& t)
+template <typename T>
+void getValue(YAML::Node&& node, T& t)
 {
-	static_assert(std::is_same_v<Node, YAML::Node>);
-
 	if (node)
 	{
 		t = node.as<T>();
