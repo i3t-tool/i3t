bf08eb8ce0ab00eca4455726936262e367b18547
Petr <felkepet@fel.cvut.cz>
2021-11-25 16:35:13 +0100

    comments to Camera and Transforms

--- Source/World/Components/Camera.h
+++ Source/World/Components/Camera.h
@@ -8,6 +8,27 @@
  * Can render in already prepared framebuffer (default configuration),
  * or into framebuffer provided by user.
  * Camera renders by calling render(glm::mat4*,bool) on each component on every GameObject in scene tree (sceneRoot).
+ *
+ * Zatím cituji èeský popis od Daniela:
+ * Co se týèe kamery, kamera nemá objekt (GameObject) jako komponentu. Kamera sama je komponenta, ale je navržena
+ * pro dvojí úèel, èímž je její interface trochu matoucí:
+ *
+ * 1. mód - Pohled do scény - kamera je komponentou gameobjectu, její transformace (view) je inverzi transformace
+ * jejího gameobjectu. Projekèní matice se nastavuje každý update() podle obrazovky a m_angle.
+ * V tomto módu kamera renderuje na obrazovku.
+ *
+ * 2. mód - Preview nìjaké geometrie - kamera není pøipnuta k žádnému gameobjectu, je k ní pøipnuta render textura.
+ * V tomto módu se projekèní matice nastaví jednou v konstruktoru. Matice view je jednotková. Matici projekce lze
+ * zmìnit pøímo dosazením, napø.:
+ * camera->m_perspective=glm::ortho...
+ *
+ * Mód kamery se nastaví podle toho, který konstruktor je použit - konstruktor bez rendertextury je první mód.
+ * V obou módech je potøeba dodat kameøe scénu, která se bude renderovat, což je parametr sceneRoot. Gameobject
+ * musí mít komponentu renderer, aby ho kamera vykreslila.
+ *
+ * Kamera má také parametr bool m_processView.
+ * Pøi  nastavení na true kamera dodává do shaderu matici view invertovanou a ortonormalizovanou,
+ * v opaèném pøípadì ji nijak nemìní. 
  */
 //---------------
 #include "../GameObject.h"
