3a711da433916a1d4f918ab3abfbe3dfd99e6a68
Petr <felkepet@fel.cvut.cz>
2021-12-04 19:51:24 +0100

    resizable screen based on current node_editor - stil crashes on

--- Source/World/RenderTexture.cpp
+++ Source/World/RenderTexture.cpp
@@ -1,7 +1,31 @@
 #include "RenderTexture.h"
 
+	bool RenderTexture::checkFramebuffer()
+	{
+		bool result = false;
+		// Check - todo remove
+		glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
+		GLenum g=glCheckFramebufferStatus(GL_FRAMEBUFFER);
+		printf("FBO ");
+		switch (g){
+			case GL_FRAMEBUFFER_UNDEFINED:									printf("undefined\n");break;
+			case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:			printf("incomplete attachment\n");break;
+			case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:	printf("missing attachment\n");break;
+			case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER:			printf("incomplete draw buffer\n");break;
+			case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER:			printf("incomplete read buffer\n");break;
+			case GL_FRAMEBUFFER_UNSUPPORTED:								printf("unsupported\n");break;
+			case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:			printf("incomplete multisample\n");break;
+			case GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS:		printf("incomplete layer targets\n");break;
+			case GL_FRAMEBUFFER_COMPLETE:										printf("complete\n"); result = true; break;
+			default:printf("--%d\n",g);break;
+		}
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		CHECK_GL_ERROR();
+		
+		return result;
+	}
 
-// TODO PF - This object is used for fiven size of the texture. Create a resize function for screen!
+// TODO PF - This object is used for a given size of the texture. Create a resize function for screen!
 RenderTexture::RenderTexture(GLuint* colorAttachment, int w, int h) 
 { 
 	m_width = w;
@@ -27,27 +51,50 @@ RenderTexture::RenderTexture(GLuint* colorAttachment, int w, int h)
 	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_depthStencilRbo);
 	glBindRenderbuffer(GL_RENDERBUFFER, 0);
 
-	glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
-	GLenum g=glCheckFramebufferStatus(GL_FRAMEBUFFER);
-	printf("FBO ");
-	switch (g){
-		case GL_FRAMEBUFFER_UNDEFINED:									printf("undefined\n");break;
-		case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:			printf("incomplete attachment\n");break;
-		case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:	printf("missing attachment\n");break;
-		case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER:			printf("incomplete draw buffer\n");break;
-		case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER:			printf("incomplete read buffer\n");break;
-		case GL_FRAMEBUFFER_UNSUPPORTED:								printf("unsupported\n");break;
-		case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:			printf("incomplete multisample\n");break;
-		case GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS:		printf("incomplete layer targets\n");break;
-		case GL_FRAMEBUFFER_COMPLETE:										printf("complete\n");break;
-		default:printf("--%d\n",g);break;
-	}
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-	CHECK_GL_ERROR();
+	checkFramebuffer();
 
 	// return value
 	*colorAttachment = m_colorTexture;  // pass the texture to ImGUI for preview
 }
+
+void RenderTexture::resize(int newWidth, int newHeight)
+{
+  assert(newWidth > 0);
+	assert(newWidth > 0);
+		
+  if(newWidth != m_width || newHeight != m_height)
+  {
+  	const float MIN_RATE = 0.5f;
+  	const float MAX_RATE = 1.2f;
+  	
+  	float rateX = static_cast<float> (newWidth)  / static_cast<float> (m_width);
+  	float rateY = static_cast<float> (newHeight) / static_cast<float> (m_height);
+  	//printf("rateX = %5.3f, rateY = %5.3f\n", rateX, rateY);
+
+		// TODO PF - find the best texture resize strategy
+  	//if( rateX < MIN_RATE || rateX > MAX_RATE || rateY < MIN_RATE || rateY > MAX_RATE)   // both directions
+  	if( rateX > MAX_RATE || rateY > MAX_RATE)  // only enlarge
+  	{
+  		printf("(%5.3i, %5.3i) -> (%5.3i, %5.3i)\n", m_width, m_height, newWidth, newHeight);
+  		
+			glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
+
+			glBindTexture(GL_TEXTURE_2D, m_colorTexture);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, newWidth, newHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+
+			glBindRenderbuffer(GL_RENDERBUFFER, m_depthStencilRbo);
+			glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, newWidth, newHeight);
+
+			glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+			//checkFramebuffer();  // TODO - what if not correct?
+
+			m_width = newWidth;
+			m_height = newHeight;		
+  	}
+  }
+}
+
 
 RenderTexture::~RenderTexture()
 {
