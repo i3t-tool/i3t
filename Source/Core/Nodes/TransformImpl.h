eb2b02c1fc39067b07e23c4efd1da0e7ccdf0f64
felkepet <felkepet@fel.cvut.cz>
2023-10-22 11:27:58 +0200

    missing indentation of a comment

--- Source/Core/Nodes/TransformImpl.h
+++ Source/Core/Nodes/TransformImpl.h
@@ -1,15 +1,15 @@
 /**
-* \file
-* \brief Implementation of individual transformation functionality
-* \authors Martin Herich <martin.herich@phire.cz>, Petr Felkel <felkel@fel.cvut.cz> (updates)
-* \copyright Copyright (C) 2016-2023 I3T team, Department of Computer Graphics
-* and Interaction, FEE, Czech Technical University in Prague, Czech Republic
-*
-* This file is part of I3T - An Interactive Tool for Teaching Transformations
-* http://www.i3t-tool.org
-*
-* GNU General Public License v3.0 (see LICENSE.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
-*
+ * \file
+ * \brief Implementation of individual transformation functions
+ * \authors Martin Herich <martin.herich@phire.cz>, Petr Felkel <felkel@fel.cvut.cz> (updates)
+ * \copyright Copyright (C) 2016-2023 I3T team, Department of Computer Graphics
+ * and Interaction, FEE, Czech Technical University in Prague, Czech Republic
+ *
+ * This file is part of I3T - An Interactive Tool for Teaching Transformations
+ * http://www.i3t-tool.org
+ *
+ * GNU General Public License v3.0 (see LICENSE.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+ *
  * Transform node
  * ==============
  * Each transform node represents a 4x4 transformation.
