040af486e0e8c81dc992cc3bf168f6e540f92fbb
Martin Herich <martin.herich@phire.cz>
2024-11-05 20:47:23 +0100

    Update CreateBundle script

--- Scripts/CreateBundle.sh
+++ Scripts/CreateBundle.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 FILES="Data* LICENSE.txt"
 
@@ -7,99 +7,112 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && 
 cd "$SCRIPT_DIR/.."
 ROOT=$(pwd)
 
-IS_WINDOWS=0
-IS_UNIX_LIKE=0
+ZIP_CMD="zip"
 
-function detect_os_type() {
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        IS_UNIX_LIKE=1
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        # Mac OSX
-        IS_UNIX_LIKE=1
-    elif [[ "$OSTYPE" == "cygwin" ]]; then
-        # POSIX compatibility layer and Linux environment emulation for Windows
-        IS_WINDOWS=1
-    elif [[ "$OSTYPE" == "msys" ]]; then
-        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-        IS_WINDOWS=1
-    elif [[ "$OSTYPE" == "win32" ]]; then
-        # I'm not sure this can happen.
-        IS_WINDOWS=1
+function default_zip_cmd() {
+    if [ -x "$(command -v zip)" ]; then
+        ZIP_CMD="zip"
+    elif [ -x "$(command -v 7z)" ]; then
+        ZIP_CMD="7z"
     else
-        # Unknown.
-        echo "Unknown OS"
+        echo "zip or 7z is not installed. Please install one of them to proceed."
         exit 1
     fi
 }
 
 CONFIG_TYPE=""
-ARCHIVE_NAME_POSTFIX=""
+ARCHIVE_NAME_SUFFIX=""
 
 function print_usage() {
-    echo "Usage: $0 [--postfix <postfix>] <config_type>"
-    echo "  --postfix <postfix> - optional postfix to add to the archive name"
-    echo "  <config_type> - Debug or Release"
+    local script_name=$(basename $0)
+
+    echo "Usage: $script_name [--suffix <suffix> ...] <config_type>"
+    echo ""
+    echo "  <config_type>       Debug, Release, MinSizeRel or RelWithDebInfo"
+    echo ""
+    echo "  --suffix <suffix>   optional suffix to add to the archive name"
+    echo "  --zip-cmd <command name>"
+    echo "                      optional zip command to use, either zip or 7z"
+    echo "  --help              print this message"
+    echo "  --debug             enable debug output"
+}
+
+# Print usage and exit.
+function exit_and_print_usage() {
+    message=$1
+    if [ ! -z "$message" ]; then
+        echo $message
+        echo ""
+    fi
+
+    print_usage
+    exit 1
 }
 
 function process_args() {
+    default_zip_cmd
+
     while [ $# -gt 0 ]; do
         case $1 in
-            --postfix)
-                ARCHIVE_NAME_POSTFIX="$ARCHIVE_NAME_POSTFIX-$2"
+            --suffix)
+                ARCHIVE_NAME_SUFFIX="-$2"
                 shift
                 shift
             ;;
-            Debug)
-                CONFIG_TYPE="Debug"
-                ARCHIVE_NAME_POSTFIX="$ARCHIVE_NAME_POSTFIX-Debug"
+            --zip-cmd)
+                ZIP_CMD="$2"
+                shift
                 shift
             ;;
-            Release)
-                CONFIG_TYPE="Release"
+            Debug|Release|MinSizeRel|RelWithDebInfo)
+                CONFIG_TYPE="$1"
+                shift
+            ;;
+            --help)
+                print_usage
+                exit 0
+            ;;
+            --debug)
+                set -x
                 shift
             ;;
             *)
-                echo "Unknown config type $1, only Debug and Release are supported."
-                echo ""
-                print_usage
-                exit 1
+                exit_and_print_usage "Unknown argument $1."
             ;;
         esac
     done
 
     if [ -z "$CONFIG_TYPE" ]; then
-        echo "Config type is not specified."
-        echo ""
-        print_usage
-        exit 1
-    else
-        echo "Config type is $CONFIG_TYPE"
+        exit_and_print_usage "Config type is not specified."
     fi
 }
 
 # Zip all files given to this function, but exclude hidden ones.
 function zip_cmd() {
-    if [ $IS_WINDOWS = 1 ]; then
-        7z a -xr'!.*' "$@"
-    else
-        zip -r "$@" -x "*/.*"
-    fi
+    case $ZIP_CMD in
+        zip)
+            zip -r "$@" -x "*/.*"
+        ;;
+        7z)
+            7z a -xr'!.*' "$@"
+        ;;
+    esac
 }
 
 function create_bundle() {
-    binaries_directory="Binaries/$CONFIG_TYPE"
+    local binaries_directory="Binaries/$CONFIG_TYPE"
 
     if [ ! -d "$binaries_directory" ]; then
         echo "Directory $binaries_directory does not exist. You have to build I3T first."
         exit 1
     fi
 
-    git_version=$(git describe --tags --exact-match HEAD 2> /dev/null || git rev-parse HEAD)
+    local git_version=$(git describe --tags --exact-match HEAD 2> /dev/null || git rev-parse HEAD)
 
-    archive_name="I3T$ARCHIVE_NAME_POSTFIX.zip"
+    local archive_name="I3T${ARCHIVE_NAME_SUFFIX}-${git_version}.zip"
 
     cp Data/imgui_default.ini Data/imgui.ini
-    binaries=$(find "$binaries_directory" -name "*.so" -o -name "*.dll" -o -name "I3T" -o -name "I3T.exe")
+    local binaries=$(find "$binaries_directory" -name "*.so" -o -name "*.so.*" -o -name "*.dll" -o -name "I3T" -o -name "I3T.exe")
     for binary in $binaries; do
         local binary_name=$(basename $binary)
 
@@ -116,6 +129,5 @@ function create_bundle() {
     zip_cmd "$archive_name" $FILES
 }
 
-detect_os_type
 process_args "$@"
 create_bundle
