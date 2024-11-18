c97bb989497217169f12e0fa255cb62f75dd3272
Martin Herich <martin.herich@phire.cz>
2024-11-18 21:00:39 +0000

    Add check format job

--- Scripts/CheckFormat.sh
+++ Scripts/CheckFormat.sh
@@ -2,5 +2,12 @@
 
 set -eo pipefail
 
-find "Source" -iname *.h -o -iname *.cpp | xargs clang-format -dry-run -Werror
-find "Test"   -iname *.h -o -iname *.cpp | xargs clang-format -dry-run -Werror
+EXECUTABLE="clang-format"
+if command -v "$EXECUTABLE"-16 &> /dev/null; then
+  EXECUTABLE="clang-format-16"
+fi
+
+echo "Using $($EXECUTABLE --version)"
+
+find "Source" -iname *.h -o -iname *.cpp | xargs "$EXECUTABLE" -dry-run -Werror
+find "Test"   -iname *.h -o -iname *.cpp | xargs "$EXECUTABLE" -dry-run -Werror
