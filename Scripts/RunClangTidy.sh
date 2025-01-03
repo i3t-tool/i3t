b4b4f7384a94600e92cc4f0a19fa40912d474f1e
Martin Herich <martin.herich@phire.cz>
2025-01-03 17:11:30 +0100

    Tryfix clang-tidy*
    Still having some diagnostic errors

--- Scripts/RunClangTidy.sh
+++ Scripts/RunClangTidy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -eu
 
-echo "OK"
-# output=$(find "Source" -iname *.h -o -iname *.cpp | xargs clang-tidy -p build-make)
+cmake -S . \
+  -B build \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+# run-clang-tidy-17 is a script that runs clang-tidy-17 in parallel.
+find "Source" -iname *.cpp | xargs run-clang-tidy-17 -j 4 -extra-arg='-stdlib=libc++' -p build
